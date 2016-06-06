192bc69938e286ee5da8490a009c18a49df149ae
codebooker <codebooker@gmail.com>
2016-06-05 22:13:42 -0400

    New Preloader

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 jQuery(function($) {
 
 	//Preloader
-	var preloader = $('.preloader');
-	$(window).load(function(){
-		preloader.remove();
-	});
+	//var preloader = $('.preloader');
+	//$(window).load(function(){
+	//	preloader.remove();
+	//});
 
+    jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+    
 	//#main-slider
 	var slideHeight = $(window).height();
 	$('#home-slider .item').css('height',slideHeight);
